90680dca0d4dd63b01363bc753428bf0c2fd916d
alexander mokryak <a.mokryak87@gmail.com>
2024-02-19 19:00:55 +0300

    Add Routes

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,11 +1,24 @@
-// import Login from '../../pages/login/Login';
-// import Main from '../pages/main/main';
-// import Room from '../../pages/room/Room';
-// import Favorites from '../../pages/favorites/Favorites';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+
+import Login from '../pages/login/Login';
+import Main from '../pages/main/main';
+import Favorites from '../pages/favorites/Favorites';
+import Room from '../pages/room/Room';
 import NotFound from '../pages/not-found/not-found';
 
+
 function App(): JSX.Element {
-  return <NotFound/>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path={'/'} element={<Main/>}/>
+        <Route path={'/login'} element={<Login/>}/>
+        <Route path={'/favorites'} element={<Favorites/>}/>
+        <Route path={'/offer/:id'} element={<Room/>}/>
+        <Route path={'*'} element={<NotFound/>}/>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
