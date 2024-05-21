13cb504fb6e028211b9959f863622aaab1286bb5
alexander mokryak <a.mokryak87@gmail.com>
2024-05-21 18:30:05 +0300

    Add React-Toastify

--- src/api.ts
+++ src/api.ts
@@ -1,8 +1,24 @@
-import axios, { AxiosInstance, AxiosRequestConfig } from 'axios';
+import axios, { AxiosInstance, AxiosRequestConfig, AxiosError, AxiosResponse } from 'axios';
 import {Token} from './utils';
+import {toast} from 'react-toastify';
+import {StatusCodes} from 'http-status-codes';
 
 const BACKEND_URL = 'https://10.react.htmlacademy.pro/six-cities';
 const REQUEST_TIMEOUT = 5000;
+
+type DetailMessageType = {
+  type: string;
+  message: string;
+}
+
+const StatusCodeMapping: Record<number, boolean> = {
+  [StatusCodes.BAD_REQUEST]: true,
+  [StatusCodes.UNAUTHORIZED]: true,
+  [StatusCodes.NOT_FOUND]: true
+};
+
+const shouldDisplayError = (response: AxiosResponse) => !!StatusCodeMapping[response.status];
+
 
 export const createAPI = (): AxiosInstance => {
   const api = axios.create({
@@ -21,6 +37,19 @@ export const createAPI = (): AxiosInstance => {
 
       return config;
     },
+  );
+
+  api.interceptors.response.use(
+    (response) => response,
+    (error: AxiosError<DetailMessageType>) => {
+      if (error.response && shouldDisplayError(error.response)) {
+        const detailMessage = (error.response.data);
+
+        toast.warn(detailMessage.message);
+      }
+
+      throw error;
+    }
   );
 
   return api;
--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,5 @@
 import {Route, Routes, unstable_HistoryRouter as HistoryRouter} from 'react-router-dom';
 import history from './history';
-import {Provider} from 'react-redux';
-import {store} from './store/store';
 
 import Login from './pages/login/login';
 import Main from './pages/main/main';
@@ -14,40 +12,38 @@ import PrivateRoute from './components/private-route/private-route';
 
 function App(): JSX.Element {
   return (
-    <Provider store={store}>
-      <HistoryRouter history={history}>
-        <Routes>
-          <Route
-            index
-            element={<Main/>}
-          />
-          <Route
-            path={AppRoute.Login}
-            element={
-              <PrivateRoute restrictedFor={AuthorizationStatus.Auth} redirectTo={AppRoute.Main}>
-                <Login/>
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path={AppRoute.Favorites}
-            element={
-              <PrivateRoute restrictedFor={AuthorizationStatus.NoAuth} redirectTo={AppRoute.Login}>
-                <Favorites/>
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path={AppRoute.Room}
-            element={<Room/>}
-          />
-          <Route
-            path={AppRoute.NotFound}
-            element={<NotFound/>}
-          />
-        </Routes>
-      </HistoryRouter>
-    </Provider>
+    <HistoryRouter history={history}>
+      <Routes>
+        <Route
+          index
+          element={<Main/>}
+        />
+        <Route
+          path={AppRoute.Login}
+          element={
+            <PrivateRoute restrictedFor={AuthorizationStatus.Auth} redirectTo={AppRoute.Main}>
+              <Login/>
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path={AppRoute.Favorites}
+          element={
+            <PrivateRoute restrictedFor={AuthorizationStatus.NoAuth} redirectTo={AppRoute.Login}>
+              <Favorites/>
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path={AppRoute.Room}
+          element={<Room/>}
+        />
+        <Route
+          path={AppRoute.NotFound}
+          element={<NotFound/>}
+        />
+      </Routes>
+    </HistoryRouter>
   );
 }
 
